d4d684dba58b51f89061d90364f4793aaa268a12
Jawad Ali <alijamil734@gmail.com>
2021-09-11 12:49:05 +0500

    made base modal

---
+++ src/components/Base/BaseModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <!--Modal-->
+  <div
+    class="fixed w-full h-full top-0 left-0 flex items-center justify-center"
+  >
+    <!-- Overlay -->
+    <div class="absolute w-full h-full bg-gray-900 opacity-50" />
+    <!-- Here starts the actual modal -->
+    <div
+      class="
+        bg-white
+        w-auth-width
+        mx-auto
+        rounded
+        shadow-lg
+        z-50
+        overflow-y-auto
+      "
+    >
+      <div class="modal-content text-left">
+        <slot name="title"></slot>
+
+        <slot></slot>
+
+        <div class="flex justify-end">
+          <slot name="action"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -4,16 +4,45 @@ import { createRouter, createWebHistory } from "vue-router"
 const routes = [
   {
     path: "/",
-    beforeEnter (to: any, from: any, next: any){
-      next("/")
+    beforeEnter(to: any, from: any, next: any) {
+      next("/blogs")
+    }
+  },
+  {
+    path: '/blogs',
+    name: 'home',
+    component: () => import("./views/Home.vue")
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import("./views/Login.vue"),
+    meta: {
+      layout: "full"
+    }
+  },
+  {
+    path: '/signup',
+    name: 'sign-up',
+    component: () => import("./views/Register.vue"),
+    meta: {
+      layout: "full"
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'page-not-found',
+    component: () => import("./views/404.vue"),
+    meta: {
+      layout: "full"
     }
   }
-];
+]
 
 
 const router = createRouter({
-	history: createWebHistory(),
-	routes,
-});
+  history: createWebHistory(),
+  routes,
+})
 
 export default router
