bb861029335fdc928e0c53bf0f4ed16c0ef52d46
Jawad Ali <alijamil734@gmail.com>
2021-09-12 11:24:21 +0500

    profile pic component

---
+++ src/components/Base/ProfilePic.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from "vue-demi";
+
+const props = defineProps({
+  photo: {
+    type: String,
+    default: "",
+  },
+  wh: {
+    type: Number,
+    default: 8
+  }
+});
+
+const pic = computed(() => props.photo || "/avatar.png");
+</script>
+<template>
+  <img
+    :src="pic"
+    :class="`w-${props.wh} h-${props.wh}`"
+    class="rounded-full cursor-pointer object-cover mr-5 mt-4"
+    alt="profile photo"
+  />
+</template>
